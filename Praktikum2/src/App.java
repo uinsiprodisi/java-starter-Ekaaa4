6d692bf021ab068c1bf3686705a137a9205e820c
Abrorrr1 <berjobber@gmail.com>
2026-04-06 14:31:01 +0800

    Tugas 2 Enculation

--- Praktikum2/src/App.java
+++ Praktikum2/src/App.java
@@ -35,14 +35,25 @@ class AyamGoreng {
 public class App {
     public static void main(String[] args) {
 
-        AyamGoreng ayamGoreng = new AyamGoreng();
+        AyamGoreng ayamGoreng1 = new AyamGoreng();
+        AyamGoreng ayamGoreng2 = new AyamGoreng();
         
-        ayamGoreng.setAyam("Ayam KFC");
-        ayamGoreng.setTepung(100);
-        ayamGoreng.setMinyak(50);
+        ayamGoreng1.setAyam("Paha Ayam");
+        ayamGoreng1.setTepung(100);
+        ayamGoreng1.setMinyak(50);
 
-        System.out.println("Ayam: " + ayamGoreng.getAyam());
-        System.out.println("Tepung: " + ayamGoreng.getTepung());
-        System.out.println("Minyak: " + ayamGoreng.getMinyak() + " ml");
+        ayamGoreng2.setAyam("Dada Ayam");
+        ayamGoreng2.setTepung(150);
+        ayamGoreng2.setMinyak(75);
+
+        System.out.println("Data Pesanan Ayam Goreng 1:");
+        System.out.println("Ayam: " + ayamGoreng1.getAyam());
+        System.out.println("Tepung: " + ayamGoreng1.getTepung());
+        System.out.println("Minyak: " + ayamGoreng1.getMinyak() + " ml");
+
+        System.out.println("Data Pesanan Ayam Goreng 2:");
+        System.out.println("Ayam: " + ayamGoreng2.getAyam());
+        System.out.println("Tepung: " + ayamGoreng2.getTepung());
+        System.out.println("Minyak: " + ayamGoreng2.getMinyak() + " ml");
     }
 }
